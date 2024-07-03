5d0abd7261332418a2922c7db9a31a6b887a38da
mahavir-dash <mahavir@swnglobal.com>
2024-07-03 20:15:47 +0530

    chore: Refactor BankAccount constructor and methods, and update tests.rs file

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ struct BankAccount {
 impl BankAccount {
     // Function to create a new bank account
     fn new(account_number: u32, initial_balance: f64) -> BankAccount {
-        BankAccount {
-            account_number,
-            balance: initial_balance,
-        }
+        todo!()
     }
 
     // Function to get account balance
@@ -21,38 +18,17 @@ impl BankAccount {
 
     // Function to deposit money
     fn deposit(self, amount: f64) -> BankAccount {
-        BankAccount {
-            account_number: self.account_number,
-            balance: self.balance + amount,
-        }
+        todo!()
     }
 
     // Function to withdraw money
     fn withdraw(self, amount: f64) -> BankAccount {
-        if amount <= self.balance {
-            BankAccount {
-                account_number: self.account_number,
-                balance: self.balance - amount,
-            }
-        } else {
-            self // Return the original account if withdrawal is not possible
-        }
+        todo!()
     }
 
     // Function to transfer money from one account to another
-    fn transfer(self, mut to_account: BankAccount, amount: f64) -> (BankAccount, BankAccount) {
-        if amount <= self.balance {
-            to_account = to_account.deposit(amount);
-            (
-                BankAccount {
-                    account_number: self.account_number,
-                    balance: self.balance - amount,
-                },
-                to_account,
-            )
-        } else {
-            (self, to_account) // Return the original accounts if transfer is not possible
-        }
+    fn transfer(self, to_account: BankAccount, amount: f64) -> (BankAccount, BankAccount) {
+        todo!()
     }
 }
 
@@ -61,22 +37,39 @@ fn main() {
     let account2 = BankAccount::new(654321, 500.0);
 
     // Print initial balances
-    println!("Initial Balance of Account 1: ${:.2}", account1.get_balance());
-    println!("Initial Balance of Account 2: ${:.2}", account2.get_balance());
+    println!(
+        "Initial Balance of Account 1: ${:.2}",
+        account1.get_balance()
+    );
+    println!(
+        "Initial Balance of Account 2: ${:.2}",
+        account2.get_balance()
+    );
 
     // Deposit money into account1
     let account1 = account1.deposit(500.0);
-    println!("Balance of Account 1 after deposit: ${:.2}", account1.get_balance());
+    println!(
+        "Balance of Account 1 after deposit: ${:.2}",
+        account1.get_balance()
+    );
 
     // Withdraw money from account1
     let account1 = account1.withdraw(300.0);
-    println!("Balance of Account 1 after withdrawal: ${:.2}", account1.get_balance());
+    println!(
+        "Balance of Account 1 after withdrawal: ${:.2}",
+        account1.get_balance()
+    );
 
     // Transfer money from account1 to account2
     let (account1, account2) = account1.transfer(account2, 200.0);
 
     // Print balances after transfer
-    println!("Balance of Account 1 after transfer: ${:.2}", account1.get_balance());
-    println!("Balance of Account 2 after transfer: ${:.2}", account2.get_balance());
+    println!(
+        "Balance of Account 1 after transfer: ${:.2}",
+        account1.get_balance()
+    );
+    println!(
+        "Balance of Account 2 after transfer: ${:.2}",
+        account2.get_balance()
+    );
 }
-
